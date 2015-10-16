70f1186a1a3fffe2f44c23bec3e0375cb747e6e1
Mimi Schatz <mcschatz@gmail.com>
2015-10-16 09:49:56 -0600

    player can select a path from the game start

--- mastermind.rb
+++ mastermind.rb
@@ -1,53 +1,54 @@
+class Runner
+  def run
+    puts "Welcome to MASTERMIND. Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    mastermind = Mastermind.new
+    response = nil
+    until response && response.status == :won
+      print "> "
+      input = gets.chomp
+      response = mastermind.execute(input)
+      puts response.message
+    end
+    puts "Goodbye!"
+  end
+end
+
+class Response
+  attr_reader :message, :status
+
+  def initialize(inputs)
+    @message = inputs[:message]
+    @status  = inputs[:status]
+  end
+end
+
 class Mastermind
-
-  def initialize(input)
-    @message = input
+  def execute(input)
+    if input == "p"
+      Response.new(message: "Let's play!")
+    elsif input == "i"
+      Response.new(message: "Some Intructions")
+    else input == "q"
+      Response.new(message: "Goodbye!")
+    end
   end
+end
 
-  def welcome(input)
-    puts "Welcome to Mastermind! Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    print "> "
-    input = input
-  end
+Runner.new.run
+#   I have generated a beginner sequence with four elements made up of: (r)ed,
+# (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+# What's your guess?
+# Guesses are case insensitive
+# If it's 'q' or 'quit' then exit the game
+# If it's 'c' or 'cheat' then print out the current secret code
+# If it's fewer than four letters, tell them it's too short
+# If it's longer than four letters, tell them it's too long
+# If they guess the secret sequence, enter the end game flow below
+# Otherwise give them feedback on the guess like this:
+# 'RRGB' has 3 of the correct elements with 2 in the correct positions
+# You've taken 1 guess
+# Congratulations! You guessed the sequence 'GRRB' in 8 guesses over 4 minutes,
+# 22 seconds.
 
-  If they enter p or play then they enter the game flow described below.
-  If they enter i or instructions they should be presented with a short explanation of how the game is played.
-  If they enter q or quit then the game should exit
-
-  welcome
-
-
-# class Mastermind
-#   attr_reader :message, :status
-
-#   def initialize(inputs)
-#     @message = inputs[:message]
-#     @status  = inputs[:status]
-#   end
-
-#   def run
-#     puts "Welcome to Mastermind"
-
-#     mastermind = Mastermind.new
-#     response = nil
-
-#     until response && response.status == :won
-#       print "> "
-#       input = gets.chomp
-#       response = mastermind.execute(input)
-#       puts response.message
-#     end
-#     puts "Goodbye!"
-#   end
-# end
-
-# def response(input)
-#   secret = "BBGB"
-#   if input == secret
-#     @message = "You Win!"
-#     @status = "won"
-#   else
-#     @message = "Guess again!"
-#     @status = "continue"
-#   end
-# end
+# Do you want to (p)lay again or (q)uit?
+# If they enter 'p' or 'play' then restart the game. 'q' or 'quit' ends the game.
