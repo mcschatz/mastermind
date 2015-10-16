e567b61b2e8321f7184adde461aa83aab20bd143
Mimi Schatz <mcschatz@gmail.com>
2015-10-16 10:45:03 -0600

    can count guesses

--- mastermind.rb
+++ mastermind.rb
@@ -3,7 +3,7 @@ class Game
     puts "Welcome to MASTERMIND. Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
     mastermind = Mastermind.new
     response = nil
-    until response && response.status == :won
+    until response && response.status == :end
       print "> "
       input = gets.chomp.downcase
       response = mastermind.execute(input)
@@ -14,7 +14,7 @@ class Game
 end
 
 class Response
-  attr_reader :message, :status
+  attr_reader :message, :status, :guesses
 
   def initialize(inputs)
     @message = inputs[:message]
@@ -23,26 +23,34 @@ class Response
 end
 
 class Mastermind
+  attr_reader :guesses
+
+  def initialize
+    @guesses = []
+  end
+
   def execute(input)
+    guesses << input
+
     if input == "p"
       Response.new(message: "Let's play! I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?")
     elsif input == "i"
-      Response.new(message: "Some Intructions")
+      Response.new(message: "To play, enter a sequence of 4 letters. Ex: ghty")
     elsif input == "q"
-      Response.new(status: :won)
+      Response.new(status: :end)
     elsif input == "c"
-      Response.new(message: "The secret code is rgby")
+      Response.new(message: "The secret code is rgby. You've taken #{@guesses.count-1} guesses!")
     elsif input.length == 4
       secret = "rgby"
       if input == secret
-        Response.new(message: "You Win!", status: :won)
+        Response.new(message: "You Win! Do you want to (p)lay again or (q)uit?")
       else
-        Response.new(message: "Guess again!")
+        Response.new(message: "Guess again! You've taken #{@guesses.count-1} guesses!")
       end
     elsif input.length < 4
-        Response.new(message: "Your guess is too short! It must be 4 characters long!")
+        Response.new(message: "Your guess is too short! It must be 4 characters long! You've taken #{@guesses.count-1} guesses!")
     else input.length > 4
-        Response.new(message: "Your guess is too long! It can only be 4 characters long!")
+        Response.new(message: "Your guess is too long! It can only be 4 characters long! You've taken #{@guesses.count-1} guesses!")
     end
   end
 end
@@ -50,10 +58,6 @@ end
 Game.new.play
 
 # Otherwise give them feedback on the guess like this:
-# 'RRGB' has 3 of the correct elements with 2 in the correct positions
-# You've taken 1 guess
+# 'RRGB' has 3 of the correct elements with 2 in the correct position
 # Congratulations! You guessed the sequence 'GRRB' in 8 guesses over 4 minutes,
 # 22 seconds.
-
-# Do you want to (p)lay again or (q)uit?
-# If they enter 'p' or 'play' then restart the game. 'q' or 'quit' ends the game.
