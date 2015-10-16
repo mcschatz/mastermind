88e05f5905a5706cd4ce284f07ab5f89286b35ae
Mimi Schatz <mcschatz@gmail.com>
2015-10-16 10:09:08 -0600

    game is working

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,11 @@
-class Runner
-  def run
+class Game
+  def play
     puts "Welcome to MASTERMIND. Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
     mastermind = Mastermind.new
     response = nil
     until response && response.status == :won
       print "> "
-      input = gets.chomp
+      input = gets.chomp.downcase
       response = mastermind.execute(input)
       puts response.message
     end
@@ -25,25 +25,30 @@ end
 class Mastermind
   def execute(input)
     if input == "p"
-      Response.new(message: "Let's play!")
+      Response.new(message: "Let's play! I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?")
     elsif input == "i"
       Response.new(message: "Some Intructions")
-    else input == "q"
-      Response.new(message: "Goodbye!")
+    elsif input == "q"
+      Response.new(status: :won)
+    elsif input == "c"
+      Response.new(message: "The secret code is rgby")
+    elsif input.length == 4
+      secret = "rgby"
+      if input == secret
+        Response.new(message: "You Win!", status: :won)
+      else
+        Response.new(message: "Guess again!")
+      end
+    elsif input.length < 4
+        Response.new(message: "Your guess is too short! It must be 4 characters long!")
+    else input.length > 4
+        Response.new(message: "Your guess is too long! It can only be 4 characters long!")
     end
   end
 end
 
-Runner.new.run
-#   I have generated a beginner sequence with four elements made up of: (r)ed,
-# (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
-# What's your guess?
-# Guesses are case insensitive
-# If it's 'q' or 'quit' then exit the game
-# If it's 'c' or 'cheat' then print out the current secret code
-# If it's fewer than four letters, tell them it's too short
-# If it's longer than four letters, tell them it's too long
-# If they guess the secret sequence, enter the end game flow below
+Game.new.play
+
 # Otherwise give them feedback on the guess like this:
 # 'RRGB' has 3 of the correct elements with 2 in the correct positions
 # You've taken 1 guess
